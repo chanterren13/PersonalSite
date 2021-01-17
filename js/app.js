7a6070f4c2afcbdc7a391c30d1f5eda82c07c2c7
terren <terren@email.com>
2021-01-17 17:26:34 -0500

    add event listeners to other nav links

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+let about = document.querySelector('.about-link');
+if(about){
+    about.addEventListener('click', function(){
+            let aboutSec = document.getElementById('about');
+            if(aboutSec){
+                let aboutY = aboutSec.getBoundingClientRect().top;
+                window.scrollTo(0, aboutY);
+            }
+    });
+}
+
 let projects = document.querySelector('.project-link');
 if(projects){
     projects.addEventListener('click', function(){
@@ -5,6 +16,17 @@ if(projects){
             if(projectSec){
                 let projY = projectSec.getBoundingClientRect().top;
                 window.scrollTo(0, projY);
+            }
+    });
+}
+
+let links = document.querySelector('.links-link');
+if(links){
+    links.addEventListener('click', function(){
+            let linkSec = document.getElementById('links');
+            if(linkSec){
+                let linkY = linkSec.getBoundingClientRect().top;
+                window.scrollTo(0, linkY);
             }
     });
 }
