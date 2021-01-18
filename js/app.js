d6a429a5b3d0c64a74c3a6ccb94a7fe4b4c821ea
terren <terren@email.com>
2021-01-17 21:01:57 -0500

    organize functions into init

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 
+init(); 
 
-window.onscroll = function() {
-    let home = document.querySelector('.home-link');
-    if(window.scrollY == 0){
-        console.log(1);
-        setActive(home);
-    }
-};
+function init(){
+    window.onscroll = function() {
+        let home = document.querySelector('.home-link');
+        if(window.scrollY == 0){
+            console.log(1);
+            setActive(home);
+        }
+    };
+
+    setUpNav();
+}
 
 function setActive(elem){
     let currActive = document.querySelector('.active');
@@ -14,38 +19,40 @@ function setActive(elem){
     elem.classList.add('active');
 }
 
-let about = document.querySelector('.about-link');
-if(about){
-    about.addEventListener('click', function(){
-            let aboutSec = document.getElementById('about');
-            if(aboutSec){
-                let aboutY = aboutSec.getBoundingClientRect().top;
-                window.scrollTo(0, aboutY);
-            }
-            setActive(about);
-    });
-}
-
-let projects = document.querySelector('.project-link');
-if(projects){
-    projects.addEventListener('click', function(){
-            let projectSec = document.getElementById('projects');
-            if(projectSec){
-                let projY = projectSec.getBoundingClientRect().top;
-                window.scrollTo(0, projY);
-            }
-            setActive(projects);
-    });
-}
-
-let links = document.querySelector('.links-link');
-if(links){
-    links.addEventListener('click', function(){
-            let linkSec = document.getElementById('links');
-            if(linkSec){
-                let linkY = linkSec.getBoundingClientRect().top;
-                window.scrollTo(0, linkY);
-            }
-            setActive(links);
-    });
+function setUpNav(){
+    let about = document.querySelector('.about-link');
+    if(about){
+        about.addEventListener('click', function(){
+                let aboutSec = document.getElementById('about');
+                if(aboutSec){
+                    let aboutY = aboutSec.getBoundingClientRect().top;
+                    window.scrollTo(0, aboutY);
+                }
+                setActive(about);
+        });
+    }
+    
+    let projects = document.querySelector('.project-link');
+    if(projects){
+        projects.addEventListener('click', function(){
+                let projectSec = document.getElementById('projects');
+                if(projectSec){
+                    let projY = projectSec.getBoundingClientRect().top;
+                    window.scrollTo(0, projY);
+                }
+                setActive(projects);
+        });
+    }
+    
+    let links = document.querySelector('.links-link');
+    if(links){
+        links.addEventListener('click', function(){
+                let linkSec = document.getElementById('links');
+                if(linkSec){
+                    let linkY = linkSec.getBoundingClientRect().top;
+                    window.scrollTo(0, linkY);
+                }
+                setActive(links);
+        });
+    }
 }
